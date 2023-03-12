03e518888f919d630274afde4b1c76d42f21a043
Artem Gonchar <artem.gonchar@softconstruct.com>
2023-03-12 22:01:14 +0200

    add allure

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 import config
 
@@ -8,15 +9,17 @@ class LoginPage:
     _LOGIN_BUTTON = "//input[@id='login-button']"
     _ERROR_MESSAGE = "//div[@class='error-message-container error']/h3"
 
+    @allure.step("Open the login page")
     def open_login_page(self, page: Page) -> None:
         page.goto(config.host.HOST_URL)
 
+    @allure.step("Login")
     def login(self, page: Page, username: str, password: str) -> None:
         page.locator(self._USERNAME_FIELD).fill(username)
         page.locator(self._PASSWORD_FIELD).fill(password)
         page.locator(self._LOGIN_BUTTON).click()
 
+    @allure.step("Validate error message: {expected_message}")
     def validate_error_message(self, page: Page, expected_message: str):
         actual_message = page.locator(self._ERROR_MESSAGE).text_content()
-        print("Actual msg: " + actual_message)
         assert actual_message == expected_message, "Error message doesn't match"
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,18 +1,27 @@
+import allure
 import pytest
 import pages
 
 
+@allure.feature("Auth")
 class TestAuth:
+
+    @allure.title("Login with valid creds")
+    @allure.severity(allure.severity_level.BLOCKER)
     def test_auth_valid_creds(self, page):
         pages.login_page.open_login_page(page)
         pages.login_page.login(page, "standard_user", "secret_sauce")
         pages.inventory_page.validate_page_title(page)
 
+    @allure.title("Login with invalid creds")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_auth_invalid_creds(self, page):
         pages.login_page.open_login_page(page)
         pages.login_page.login(page, "standard_user", "invalid_password")
-        pages.login_page.validate_error_message(page, "Epic sadface: Username and password do not match any user in this service")
+        pages.login_page.validate_error_message(page, "Epic sadface: Username and password do not match any user in this service1")
 
+    @allure.title("Locked user")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_locked_user(self, page):
         pages.login_page.open_login_page(page)
         pages.login_page.login(page, "locked_out_user", "secret_sauce")
